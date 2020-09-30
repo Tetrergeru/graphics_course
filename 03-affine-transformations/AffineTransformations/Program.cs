379f5228d175cf39ba89619472d7204a97b986f7
drKazeev <dr.kazeev@yandex.ru>
2020-09-30 10:15:16 +0300

    add rotate and move

--- 03-affine-transformations/AffineTransformations/Program.cs
+++ 03-affine-transformations/AffineTransformations/Program.cs
@@ -26,6 +26,8 @@ namespace GraphFunc
                 new ScaleTool(),
                 new ScaleCenterTool(),
                 new IntersectTool(),
+                new DrawingToolWrapper(new RotateTool()),
+                new DrawingToolWrapper(new MoveTool()),
             });
             Application.Run(form);
         }
--- 03-affine-transformations/AffineTransformations/Tools/MoveTool.cs
+++ 03-affine-transformations/AffineTransformations/Tools/MoveTool.cs
@@ -2,21 +2,21 @@
 
 namespace GraphFunc.Tools
 {
-    public class RotateTool : ITool
+    public class MoveTool : ITool
     {
         public void OnSelect(PolygonContainer polygonContainer)
         {
-            polygonContainer.Selected.Rotate(new PointF(100, 100), 90);
+            polygonContainer.Selected.Move(new PointF(2, 2));
         }
 
         public void OnUse(PolygonContainer polygonContainer, Point point)
         {
         }
-        
+
         public bool CanUseInField()
             => true;
-        
+
         public override string ToString()
-            => "Rotate";
+            => "Move";
     }
 }
