88ac1ddc39f35a3844f95b0b9c656a2e0c9a2c3f
drKazeev <dr.kazeev@yandex.ru>
2020-10-20 12:04:30 +0300

    add models
    !FIXME add always copy

--- 06-3d-1/Lab6/Form.cs
+++ 06-3d-1/Lab6/Form.cs
@@ -25,6 +25,10 @@ namespace GraphFunc
             "Models/Skull.obj",
             "Models/Prism.obj",
             "Models/Cat.obj",
+            "Models/Tetrahedron.obj",
+            "Models/Octahedron.obj",
+            "Models/Dodecahedron.obj.obj",
+            "Models/Icosahedron.obj",
         };
         
         private int _currentModel;
