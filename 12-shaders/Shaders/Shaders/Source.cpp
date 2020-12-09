325314fdd20c8539dd700d180c64b1be6fdbc3d4
drKazeev <dr.kazeev@yandex.ru>
2020-12-09 15:09:11 +0300

    add vbo cube

--- 12-shaders/Shaders/Shaders/Source.cpp
+++ 12-shaders/Shaders/Shaders/Source.cpp
@@ -7,15 +7,39 @@
 #include <functional>
 #include <conio.h>
 #include <iostream>
+//#include <gl/glm/trigonometric.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 //--------------------------------начало Shader--------------------------------
 //! ID шейдерной программы
 GLuint Program;
 //! ID атрибута
 GLint Attrib_vertex;
+GLint Attrib_color;
 //! ID юниформ переменной цвета
 GLint Unif_color;
 
 GLint Unif_angle;
+
+GLuint VBO_vertex;
+GLuint VBO_color;
+GLuint VBO_element;
+
+GLint Indices_count;
+GLint Unif_matrix;
+
+float angle = 0.25;
+double rotate_xx = 0;
+double rotate_yy = 0;
+double rotate_zz = 0;
+
+glm::mat4 Matrix_projection;
+
+struct vertex
+{
+	GLfloat x;
+	GLfloat y;
+	GLfloat z;
+};
 
 //! Проверка ошибок OpenGL, если есть то вывод в консоль тип ошибки
 void checkOpenGLerror() 
@@ -24,6 +48,76 @@ void checkOpenGLerror()
 	if ((errCode = glGetError()) != GL_NO_ERROR)
 		std::cout << "OpenGl error! - " << gluErrorString(errCode);
 }
+
+void initVBO()
+{
+	GLfloat vertices[] =
+	{
+		-1.0f , -1.0f , -1.0f ,
+		1.0f , -1.0f , -1.0f ,
+		1.0f , 1.0f , -1.0f ,
+		-1.0f , 1.0f , -1.0f ,
+		-1.0f , -1.0f , 1.0f ,
+		1.0f , -1.0f , 1.0f ,
+		1.0f , 1.0f , 1.0f ,
+		-1.0f , 1.0f , 1.0f
+	};
+
+	GLfloat colors[] =
+	{
+		1.0f , 0.5f , 1.0f ,
+		1.0f , 0.5f , 0.5f ,
+		0.5f , 0.5f , 1.0f ,
+		0.0f , 1.0f , 1.0f ,
+		1.0f , 0.0f , 1.0f ,
+		1.0f , 1.0f , 0.0f ,
+		1.0f , 0.0f , 1.0f ,
+		0.0f , 1.0f , 1.0f
+	};
+
+
+	GLint indices[] =
+	{
+		0, 4, 5,
+		0, 5, 1,
+		1, 5, 6,
+		1, 6, 2,
+		2, 6, 7,
+		2, 7, 3,
+		3, 7, 4,
+		3, 4, 0,
+		4, 7, 6,
+		4, 6, 5,
+		3, 0, 1,
+		3, 1, 2
+	};
+
+	Indices_count = sizeof(indices) / sizeof(indices[0]);
+
+	glGenBuffers(1, &VBO_vertex);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_vertex);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &VBO_color);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_color);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &VBO_element);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VBO_element);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	checkOpenGLerror();
+}
+
+void freeVBO()
+{
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glDeleteBuffers(1, &VBO_vertex);
+	glDeleteBuffers(1, &VBO_element);
+	glDeleteBuffers(1, &VBO_color);
+}
+
 //! Инициализация шейдеров
 void initShaderStrips()
 {
@@ -209,24 +303,24 @@ void initShaderTriangle()
 void initShaderCube()
 {
 	const char* vsSource =
-		"attribute vec2 coord;\n"
+		"attribute vec3 coord;\n"
+		"attribute vec3 color;\n"
+		"varying vec3 var_color;\n"
+		"uniform mat4 matrix;\n"
 		"void main() {\n"
-		"  gl_Position = vec4(coord, 0.0, 1.0);\n"
+		"gl_Position = matrix * vec4(coord , 1.0);\n"
+		"var_color = color;\n"
+		"}\n";
+	const char* fsSource =
+		"varying vec3 var_color;\n"
+		"void main() {\n"
+		"gl_FragColor = vec4(var_color , 1.0);\n"
 		"}\n";
 
-	const char* fsSource =
-		"uniform vec4 color;\n"
-		"void main() {\n"
-		" if (mod(gl_FragCoord.x, 30)<15.0) \n"
-		" gl_FragColor = color;\n"
-		" else\n"
-		" gl_FragColor = vec4(1.0,1.0,1.0,0.0);\n"
-		"}\n";
 	GLuint vShader, fShader;
 	vShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vShader, 1, &vsSource, NULL);
 	glCompileShader(vShader);
-	std::cout << "vertex shader \n";
 	fShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fShader, 1, &fsSource, NULL);
 	glCompileShader(fShader);
@@ -242,20 +336,28 @@ void initShaderCube()
 		std::cout << "error attach shaders \n";
 		return;
 	}
-	const char* attr_name = "coord";
-	Attrib_vertex = glGetAttribLocation(Program, attr_name);
+
+	Attrib_vertex = glGetAttribLocation(Program, "coord");
 	if (Attrib_vertex == -1)
 	{
-		std::cout << "could not bind attrib " << attr_name << std::endl;
+		std::cout << "could not bind attrib " << "coord" << std::endl;
 		return;
 	}
-	const char* unif_name = "color";
-	Unif_color = glGetUniformLocation(Program, unif_name);
+
+	Attrib_color = glGetAttribLocation(Program, "color");
 	if (Unif_color == -1)
 	{
-		std::cout << "could not bind uniform " << unif_name << std::endl;
+		std::cout << "could not bind uniform " << "color" << std::endl;
 		return;
 	}
+
+	Unif_matrix = glGetUniformLocation(Program, "matrix");
+	if (Unif_matrix == -1)
+	{
+		std::cout << "could not bind uniform " << "matrix" << std::endl;
+		return;
+	}
+
 	checkOpenGLerror();
 }
 
@@ -330,18 +432,52 @@ void RenderTriangle()
 	glutSwapBuffers();
 }
 
+
+
+
 void RenderSolidCube()
 {
-	
+	glMatrixMode(GL_MODELVIEW);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
 
+	glRotatef(rotate_xx, 1.0, 0.0, 0.0);
+	glRotatef(rotate_yy, 0.0, 1.0, 0.0);
+	glRotatef(rotate_zz, 0.0, 0.0, 1.0);
+	glUseProgram(Program);
+	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+
+	glm::mat4 View = glm::lookAt(glm::vec3(4, 3, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+
+	Matrix_projection = Projection * View;
+
+	glUseProgram(Program);
+
+	glUniformMatrix4fv(Unif_matrix, 1, GL_FALSE, &Matrix_projection[0][0]);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VBO_element);
+	glEnableVertexAttribArray(Attrib_vertex);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_vertex);
+
+	glVertexAttribPointer(Attrib_vertex, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glEnableVertexAttribArray(Attrib_color);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_color);
+	glVertexAttribPointer(Attrib_color, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glDrawElements(GL_TRIANGLES, Indices_count, GL_UNSIGNED_INT, 0);
+
+	glDisableVertexAttribArray(Attrib_vertex);
+	glDisableVertexAttribArray(Attrib_color);
+	glutSwapBuffers();
 }
 
 void InitFuncVector()
 {
 
 	//vFunc.push_back(RenderRectangle);
-	vFunc.push_back(RenderTriangle);
-	//vFunc.push_back(RenderSolidCube);
+	//vFunc.push_back(RenderTriangle);
+	vFunc.push_back(RenderSolidCube);
 }
 
 void Update(void)
@@ -367,14 +503,21 @@ void Reshape(int width, int height)
 	gluPerspective(65.0f, w / h, 1.0f, 1000.0f);
 }
 
+
+
+
 int main(int argc, char* argv[])
 {
+	setlocale(0, "");
 	InitFuncVector();
 	glutInit(&argc, argv);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(600, 600);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA | GLUT_ALPHA);
 	glutCreateWindow("OpenGL");
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
 
 	GLenum glew_status = glewInit();
 	if (GLEW_OK != glew_status)
@@ -390,14 +533,17 @@ int main(int argc, char* argv[])
 
 	//initShaderStrips();
 	//initShaderSquares();
-	initShaderTriangle();
-	//initShaderCube();
+	//initShaderTriangle();
+	initVBO();
+	initShaderCube();
 
 	glutIdleFunc(Update);
 	glutDisplayFunc(Update);
 	glutReshapeFunc(Reshape);
+	//glutSpecialFunc(specialKeys);
 	glutMainLoop();
 
-	freeShader();
+	//freeShader();
+	freeVBO();
 	return 0;
 }
